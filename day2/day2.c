5be9c64af3819d5c2d130acff4bb0e9092f688e9
Hayden Johnson <hajohn100@gmail.com>
2021-12-06 15:43:31 -0800

    finished part 2

--- day2/day2.c
+++ day2/day2.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char** argv)
+void part1(char* filename)
 {
-    FILE* f = fopen(argv[1], "r");
+    FILE* f = fopen(filename, "r");
     char* line = NULL;
     size_t size;
 
@@ -33,5 +33,46 @@ int main(int argc, char** argv)
     free(line);
     fclose(f);
     printf("%d\n", depth * forward);
+}
+
+void part2(char* filename)
+{
+    FILE* f = fopen(filename, "r");
+    char* line = NULL;
+    size_t size;
+
+    int forward = 0;
+    int depth = 0;
+    int aim = 0;
+
+    while(getline(&line, &size, f) > 0)
+    {
+        char* token = strtok(line, " ");
+        if (!strcmp(token, "forward"))
+        {
+            token = strtok(NULL, " ");
+            forward += atoi(token);
+            depth += aim * atoi(token);
+        }
+        else if (!strcmp(token, "up"))
+        {
+            token = strtok(NULL, " ");
+            aim -= atoi(token);
+        }
+        else if (!strcmp(token, "down"))
+        {
+            token = strtok(NULL, " ");
+            aim += atoi(token);
+        }
+    }
+    free(line);
+    fclose(f);
+    printf("%d\n", depth * forward);
+}
+
+int main(int argc, char** argv)
+{
+    part1(argv[1]);
+    part2(argv[1]);
     return 1;
 }
